19bd38ed5fe7bfabf0801f8e5bdd63d4b4342a3c
Lukyanava Maryia <Lukyanava_MA@itd.iba.by>
2018-03-07 13:34:01 +0300

    post put get delete requests

--- src/main/java/iba/controller/BuoyRestController.java
+++ src/main/java/iba/controller/BuoyRestController.java
@@ -1,12 +1,14 @@
 package iba.controller;
 
 import iba.entity.Buoy;
+import iba.exception.ResourceNotFoundException;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.HttpStatus;
-import org.springframework.http.MediaType;
+import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.*;
 import iba.repository.BuoyRepository;
 
+import javax.validation.Valid;
 import java.util.Collection;
 import java.util.Optional;
 
@@ -18,18 +20,47 @@ public class BuoyRestController {
     @Autowired
     private BuoyRepository buoyRepository;
 
-//    @GetMapping(produces = MediaType.APPLICATION_JSON_VALUE)
-    @RequestMapping(value = "/{idbuoy}", method = RequestMethod.GET)
+    @RequestMapping(value = "/buoys/{id}", method = RequestMethod.GET)
     @ResponseStatus(HttpStatus.OK)
-    public Optional<Buoy> getBuoy(@PathVariable("idbuoy") Long idbuoy) {
-        Optional<Buoy> b = buoyRepository.findById(idbuoy);
+    public Optional<Buoy> getBuoy(@PathVariable("id") Long id) {
+        Optional<Buoy> b = buoyRepository.findById(id);
         return b;
     }
 
-    @GetMapping(produces = MediaType.APPLICATION_JSON_VALUE)
-    @RequestMapping(value = "/all", method = RequestMethod.GET)
+    @RequestMapping(value = "/buoys", method = RequestMethod.GET)
     @ResponseStatus(HttpStatus.OK)
     public Collection<Buoy> getBuoys () {
         return buoyRepository.findAll();
     }
+
+    @PostMapping("/buoys")
+    public Buoy createBuoy (@Valid @RequestBody Buoy buoy) {
+        return buoyRepository.save(buoy);
+    }
+
+    @PutMapping("/buoys/{id}")
+    public Buoy updateBuoy(@PathVariable(value = "id") Long id,
+                           @Valid @RequestBody Buoy buoyDetails) {
+
+        Buoy buoy = buoyRepository.findById(id)
+                .orElseThrow(() -> new ResourceNotFoundException("Buoy", "id", id));
+
+        buoy.setLatitude(buoyDetails.getLatitude());
+        buoy.setLongitude(buoyDetails.getLongitude());
+        buoy.setOwner(buoyDetails.getOwner());
+
+        Buoy updatedBuoy = buoyRepository.save(buoy);
+        return updatedBuoy;
+    }
+
+    @DeleteMapping("/buoys/{id}")
+    public ResponseEntity<?> deleteNote(@PathVariable(value = "id") Long id) {
+        Buoy buoy = buoyRepository.findById(id)
+                .orElseThrow(() -> new ResourceNotFoundException("Buoy", "id", id));
+
+        buoyRepository.delete(buoy);
+
+        return ResponseEntity.ok().build();
+    }
+
 }
